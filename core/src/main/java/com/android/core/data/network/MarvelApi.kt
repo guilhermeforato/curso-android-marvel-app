5dea7873bbc5167d6c39a06a41f2394d8a526e15
gsouzahh <103123750+guilhermeforato@users.noreply.github.com>
2022-05-17 23:23:45 -0300

    configurando retrofit

---
+++ core/src/main/java/com/android/core/data/network/MarvelApi.kt
@@ -0,0 +1,15 @@
+package com.android.core.data.network
+
+import com.android.core.data.network.response.DataWrapperResponse
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface MarvelApi {
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @QueryMap
+        queries: Map<String, String>
+    ): DataWrapperResponse
+
+}
